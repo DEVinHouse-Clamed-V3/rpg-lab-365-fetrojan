9acea77949618e8d2a7964bec833e9902049c818
Felipe Rossi Trojan <felipetrojan@hotmail.com>
2024-11-25 09:55:06 -0300

    feat: adiciona classe Personagem

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import Arma from "./classes/Arma";
+import Personagem from "./classes/Personagem";
 
 const espada = new Arma('Espada Longa', 'Uma espada longa e afiada', 2000)
+
+const meuPersonagem = new Personagem('Felipe')
